08e46289d9172bb710d658e701dcc3e084144ae0
Wes Higbee <wes.mcclure@gmail.com>
2016-09-28 01:16:09 -0400

    Register all services needed with AddIdentityWithMongoStores(connectionString) - Update connection  string with a mongodb example

--- AspNetCore.WebApp.MongoDB/src/AspNetCore.WebApp.MongoDB/Startup.cs
+++ AspNetCore.WebApp.MongoDB/src/AspNetCore.WebApp.MongoDB/Startup.cs
@@ -4,12 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using AspNetCore.WebApp.MongoDB.Data;
 using AspNetCore.WebApp.MongoDB.Models;
 using AspNetCore.WebApp.MongoDB.Services;
 
@@ -39,13 +37,9 @@ namespace AspNetCore.WebApp.MongoDB
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+	        // Register identity framework services and also Mongo storage. 
+	        services.AddIdentityWithMongoStores(Configuration.GetConnectionString("DefaultConnection"))
+		        .AddDefaultTokenProviders();
 
             services.AddMvc();
 
