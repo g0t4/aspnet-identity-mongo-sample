43552212bcd2d306a2dfa21575a4cf27d5c0f6e8
Wes Higbee <wes.mcclure@gmail.com>
2016-09-28 01:32:05 -0400

    Add using to _ViewImports for all views to use IdentityUser type

--- AspNetCore.WebApp.MongoDB/src/AspNetCore.WebApp.MongoDB/Startup.cs
+++ AspNetCore.WebApp.MongoDB/src/AspNetCore.WebApp.MongoDB/Startup.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
