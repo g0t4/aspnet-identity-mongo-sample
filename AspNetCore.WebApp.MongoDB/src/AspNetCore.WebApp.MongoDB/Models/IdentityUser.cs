61b63d0922b5f0e58bc99cf3052c7fcd26d370d9
Wes Higbee <wes.mcclure@gmail.com>
2016-09-28 01:12:30 -0400

    Delete IdentityUser, sample will not use custom user type

--- AspNetCore.WebApp.MongoDB/src/AspNetCore.WebApp.MongoDB/Models/IdentityUser.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AspNetCore.WebApp.MongoDB.Models
-{
-	// Add profile data for application users by adding properties to the IdentityUser class
-	public class IdentityUser : Microsoft.AspNetCore.Identity.MongoDB.IdentityUser
-	{
-	}
-}
